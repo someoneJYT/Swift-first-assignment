83ce28233d9bfa62d024b2d83ccb2a102b3c92b8
someoneJYT <stmsomeone@gmail.com>
2026-03-14 18:12:33 +0800

    initial codebreaker

---
+++ Codebreaker/Codebreaker/CodebreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodebreakerApp.swift
+//  Codebreaker
+//
+//  Created by 曾禎攸 on 2026/3/14.
+//
+
+import SwiftUI
+
+@main
+struct CodebreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codebreaker/Codebreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Codebreaker
+//
+//  Created by 曾禎攸 on 2026/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
