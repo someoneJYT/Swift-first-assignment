18bd1347326625e7f878c10f7d35ca354ad88829
someoneJYT <stmsomeone@gmail.com>
2026-03-14 15:39:56 +0800

    Initial Commit: Start CS193p Memorize App

---
+++ memorize game/memorize game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  memorize game
+//
+//  Created by 曾禎攸 on 2026/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ memorize game/memorize game/memorize_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_gameApp.swift
+//  memorize game
+//
+//  Created by 曾禎攸 on 2026/3/14.
+//
+
+import SwiftUI
+
+@main
+struct memorize_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
